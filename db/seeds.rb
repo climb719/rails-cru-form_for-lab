050b0aa8d41ec061e95456920d5b364e4438fe80
climb719 <mary.oldweiler@gmail.com>
2021-05-22 11:32:44 -0400

    commit tree 026cb045c1e2a8d8f5ac3b02697d5be9426230c4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artists = Artist.create([{name: "Nirvana", bio: "American rock band"}, {name: "U2", bio: "Irish rock band from Dublin"}, {name: "Aretha Franklin", bio: "American singer and civil rights activist"}])
+genres = Genre.create([{name: "Grunge"}, {name: "Rock"}, {name: "Soul"}])
+songs = Song.create([{name: "Smells Like Teen Spirit", artist: artists.first, genre: genres.first}, {name: "Respect", artist: artists.last, genre: genres.last}])
